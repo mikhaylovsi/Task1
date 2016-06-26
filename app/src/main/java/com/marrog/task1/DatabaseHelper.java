f9840c34b083550ece66c288670d70d45073a516
mikhaylovsi <marrog1@mail.ru>
2016-06-26 22:59:59 +0300

    first update

---
+++ app/src/main/java/com/marrog/task1/DatabaseHelper.java
@@ -0,0 +1,55 @@
+package com.marrog.task1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Сергей on 22.06.2016.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "mydatabase.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String DATABASE_TABLE = "messages";
+
+    public static final String ID_COLUMN = "_id";
+    public static final String MESSAGE_TITTLE_COLUMN = "message_title";
+    public static final String MESSAGE_BODY_COLUMN = "message_body";
+    public static final String MESSAGE_DATE_COLUMN = "message_date";
+
+    public static final String DATABASE_CREATE_SCRIPT =
+            "create table " + DATABASE_TABLE + " (" + ID_COLUMN + " integer primary key autoincrement, " +
+                    MESSAGE_TITTLE_COLUMN + " text, " + MESSAGE_BODY_COLUMN + " text, " +
+                    MESSAGE_DATE_COLUMN + " integer);";
+
+
+
+
+    DatabaseHelper(Context context){
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+      db.execSQL(DATABASE_CREATE_SCRIPT);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        Log.w("SQLite", "Обновляемся с версии " + oldVersion + " на версию " + newVersion);
+
+        db.execSQL("DROP TABLE IF IT EXISTS " + DATABASE_NAME);
+
+        onCreate(db);
+
+    }
+}
---
+++ app/src/main/java/com/marrog/task1/MainActivity.java
@@ -0,0 +1,96 @@
+package com.marrog.task1;
+
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper mDataBaseHelper;
+    private SQLiteDatabase mdb;
+    private Cursor mCursor;
+
+       @Override
+    protected void onCreate(Bundle savedInstanceState) {
+           super.onCreate(savedInstanceState);
+           setContentView(R.layout.activity_main);
+
+           mDataBaseHelper = new DatabaseHelper(this);
+           mdb = mDataBaseHelper.getWritableDatabase();
+
+           ContentValues values = new ContentValues();
+           // Задайте значения для каждого столбца
+//           values.put(DatabaseHelper.MESSAGE_TITTLE_COLUMN, "Test");
+//           values.put(DatabaseHelper.MESSAGE_BODY_COLUMN, "Test");
+//           values.put(DatabaseHelper.MESSAGE_DATE_COLUMN, 5);
+//           // Вставляем данные в таблицу
+//           mdb.insert(DatabaseHelper.DATABASE_TABLE, null, values);
+
+//                   ContentValues values = new ContentValues();
+//           // Задайте значения для каждого столбца
+//           values.put(DatabaseHelper.MESSAGE_TITTLE_COLUMN, "Test1");
+//           values.put(DatabaseHelper.MESSAGE_BODY_COLUMN, "Test");
+//           values.put(DatabaseHelper.MESSAGE_DATE_COLUMN, 5);
+//           // Вставляем данные в таблицу
+//           mdb.update(DatabaseHelper.DATABASE_TABLE,values, DatabaseHelper.MESSAGE_TITTLE_COLUMN + " = ?", new String[]{"Test"});
+
+
+//           Cursor cursor = mdb.query(DatabaseHelper.DATABASE_TABLE,
+//                   new String[]{DatabaseHelper.MESSAGE_TITTLE_COLUMN, DatabaseHelper.MESSAGE_BODY_COLUMN},
+//                   null, null, null, null, null);
+
+            mCursor = mdb.query(DatabaseHelper.DATABASE_TABLE,
+                   new String[]{DatabaseHelper.MESSAGE_TITTLE_COLUMN},
+                   null, null, null, null, null);
+
+
+
+//           cursor.moveToFirst();
+//           while(cursor.moveToNext()){
+//
+//               System.out.println(cursor.getString(0));
+//
+//           }
+
+//           SimpleCursorAdapter mSCAdapter = new SimpleCursorAdapter(this, R.layout.item, cursor,
+//                   new String[]{DatabaseHelper.MESSAGE_TITTLE_COLUMN, DatabaseHelper.MESSAGE_BODY_COLUMN},
+//                   new int[]{R.id.message_title, R.id.message_body}, 1);
+
+                      SimpleCursorAdapter mSCAdapter = new SimpleCursorAdapter(this, R.layout.item, mCursor,
+                   new String[]{DatabaseHelper.MESSAGE_TITTLE_COLUMN},
+                   new int[]{R.id.message_title}, 0);
+
+           ListView listView = (ListView)findViewById(R.id.listView);
+
+           listView.setAdapter(mSCAdapter);
+
+
+
+       }
+
+
+
+
+
+
+    public void onClick(View view) {
+
+
+    }
+}
